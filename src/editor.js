f50924b36fa10a7e6ac651a8d863f10d8e652225
Aryan <dragonmonkey757@gmail.com>
2026-03-23 23:23:50 -0500

    Pull in changes for editor update

--- src/editor.js
+++ src/editor.js
@@ -1,5 +1,6 @@
 import { EditorState } from '@codemirror/state'
 import { EditorView, lineNumbers } from '@codemirror/view'
+import { basicSetup } from 'codemirror'
 import { python } from '@codemirror/lang-python'
 import { oneDark } from '@codemirror/theme-one-dark'
 import './style.css'
@@ -7,12 +8,27 @@ import './style.css'
 const state = EditorState.create({
   doc: '',
   extensions: [
+    basicSetup, 
     lineNumbers(),
     python(),
     oneDark,
   ]
 })
-new EditorView({
-  state, 
-  parent:document.getElementById('editor')
-})
+
+const editor = new EditorView({
+  state,
+  parent: document.getElementById('editor')
+});
+
+const playBtn = document.getElementById("playBtn");
+
+playBtn.addEventListener("click", () => {
+  const code = editor.state.doc.toString();
+
+  localStorage.setItem("savedCode", code);
+
+  console.log("Saved Code:");
+  console.log(code);
+
+  alert("Code saved! Check console.");
+});
